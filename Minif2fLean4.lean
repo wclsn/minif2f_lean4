97b595bd2987f40501aff674c4ae3a7711b06bf5
wclsn <1574779697@qq.com>
2024-04-30 05:51:10 +0000

    initialize this repo

---
+++ Minif2fLean4.lean
@@ -0,0 +1,5 @@
+-- This module serves as the root of the `Minif2fLean4` library.
+-- Import modules here that should be built as part of the library.
+import «Minif2fLean4».minif2f_import
+import «Minif2fLean4».test
+import «Minif2fLean4».validation
